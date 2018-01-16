fb313617c993c8d3103cd5a1d8d347c1af199bab
fostertn <fostertn@rose-hulman.edu>
2018-01-16 18:36:27 -0500

    m7 done

--- src/m7_more_mutation.py
+++ src/m7_more_mutation.py
@@ -4,8 +4,8 @@ by changing the values of instance variables.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Tyler Foster.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -157,6 +157,10 @@ def run_test_RETURN_circle():
 
 
 def RETURN_circle(circle, color):
+    new_circle = circle.clone()
+    new_circle.fill_color = color
+
+    return new_circle
     """
     Returns a NEW rg.Circle that is a CLONE (copy) of the given circle,
     but with its color set to the given color.
@@ -169,7 +173,7 @@ def RETURN_circle(circle, color):
     where the color is either a string that Rosegraphics understands
     or a rg.Color object.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
@@ -290,6 +294,8 @@ def run_test_MUTATE_circle():
 
 
 def MUTATE_circle(circle, color, n):
+    circle.fill_color = color
+    circle.center.x = circle.center.x + n
     """
     MUTATES the given circle, so that:
       1. its fill color becomes the given color, and
@@ -301,7 +307,7 @@ def MUTATE_circle(circle, color, n):
     Precondition: The first argument is a rg.Circle and the second
                   argument is a color appropriate for Zellegraphics.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
